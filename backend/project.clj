f246e3429aac3182e1aa633eeacd624e3c589ce4
Toni Tienda <tonitienda@gmail.com>
2022-07-16 18:29:59 +0900

    remove clojure backend

--- backend/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject empty/lein-template "1.0.0"
-  :description "Absolute bare minimum Leiningen template"
-  :url "https://github.com/radian-software/empty"
-  :license {:name "MIT License"
-            :url "http://www.opensource.org/licenses/mit-license.php"}
-  :eval-in-leiningen true)
