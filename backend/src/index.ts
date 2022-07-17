ac5c21280d050fa6f6e7e08b137be81f2a90c9b7
Toni Tienda <tonitienda@gmail.com>
2022-07-17 19:45:19 +0900

    code: add healthcheck endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const PORT = 3000;
 
 const app = express()
 
+app.get('/healthz', (_: Request, res: Response) => {
+    console.log("✅ healthz checked")
+    res.send("✅")
+})
+
 app.listen(PORT, () => {
-    console.log(`Listening to ${PORT}`)
+    console.log(`🚀 Listening to ${PORT}`)
 })
