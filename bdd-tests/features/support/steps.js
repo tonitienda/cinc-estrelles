4d1618507afad10ae28a429c1e074b960555e111
Toni Tienda <tonitienda@gmail.com>
2022-07-23 14:40:46 +0900

    oneoff: ignore some file that should not be pushed to git

--- bdd-tests/features/support/steps.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cucumber_1 = require("@cucumber/cucumber");
-(0, cucumber_1.When)('the greeter says hello', function () {
-    console.log('Hello');
-});
-(0, cucumber_1.Then)('I should have heard {string}', function (expectedResponse) {
-    console.log(`Heard hello`);
-});
