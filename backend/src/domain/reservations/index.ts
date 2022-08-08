50a9eae7b9f32f2652312322f594a9c2de15e3a4
Toni Tienda <tonitienda@gmail.com>
2022-08-08 22:29:03 +0900

    code: import reservations and reservation request
    - If a correct reservation is received it is stored in the DB
    - If there are some errors in the reservation input data it will be stored as a
      requested reservation until a human agent fixes the format
    
    This commit adds some bootstrapping of the connection to the infrastructure like the
    database or the message broker.

--- backend/src/domain/reservations/index.ts
+++ backend/src/domain/reservations/index.ts
@@ -1,25 +1,20 @@
 import { Router } from "express";
 import * as db from "../../tools/db-client";
+import * as broker from "../../tools/broker";
 
 // TODO - Import from module. Doing .. out of the boundaries of
 // reservations seems not correct
 import { handleCommandRequest } from "../../handlers/command";
-import { importReservation } from "./import-reservation";
+import * as importReservation from "./import-reservation";
 
-const router = Router();
+type ReservationDependencies = importReservation.Dependencies;
 
-// Bootstrapping the actual dependencies for all commands
-// The type of the dependencies for the commands are defined on each command
-// Here we will need all the dependencies. If something is missing or
-// the types do not match between the provided and the expected dependencies
-// the compiler will complain about it
-const defaultDependencies = {
-  dbClient: db.connectClient(),
+export const get = (dependencies: ReservationDependencies) => {
+  const router = Router();
+
+  router.post(
+    "/import-reservation",
+    handleCommandRequest(importReservation.execute(dependencies))
+  );
+  return router;
 };
-
-router.post(
-  "/import-reservation",
-  handleCommandRequest(importReservation(defaultDependencies))
-);
-
-export default router;
