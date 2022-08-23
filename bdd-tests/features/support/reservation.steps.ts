57536fc2eda19e878baeccfae0afa3bdb8f96399
Toni Tienda <tonitienda@gmail.com>
2022-08-24 08:38:15 +0900

    test: verify that events are sent

--- bdd-tests/features/support/reservation.steps.ts
+++ bdd-tests/features/support/reservation.steps.ts
@@ -1,10 +1,39 @@
-import { When, Then } from "@cucumber/cucumber";
+import { When, Then, Before, BeforeAll, AfterAll } from "@cucumber/cucumber";
 import axios from "axios";
 import { strict as assert } from "assert";
+import * as broker from "./tools/broker";
 
-// TODO - See this. State should be initialized before every test runs
-//        and should not be shared to allow paralel execution
-let world: any = {};
+type World = {
+  events: { topic: string; data: any }[];
+  // TODO : Define type
+  result: any | null;
+  broker: any;
+};
+
+let world: World = { events: [], result: null, broker: null };
+
+BeforeAll(async () => {
+  world = {
+    events: [],
+    result: null,
+    broker: null,
+  };
+
+  world.broker = await broker.connect();
+
+  world.broker.subscribe("reservation_events", (topic: string, data: any) => {
+    world.events.push({ topic, data });
+  });
+});
+
+Before(async () => {
+  world.events = [];
+  world.result = null;
+});
+
+AfterAll(() => {
+  return world.broker.close();
+});
 
 const ignored = (str: string) => str === "<ignore>";
 const tryNumber = (str: string) => {
@@ -71,4 +100,13 @@ Then("the reservation should be found", async () => {
 
 Then("the status2 should be {int}", (status) => {
   assert.strictEqual(world.result.status, status);
+});
+
+Then("the reservation event should have been received", () => {
+  const reservationId = world.result.data.id;
+  const event = world.events.filter(
+    (e) => e.topic === "reservation_events" && e.data.body.id === reservationId
+  );
+
+  assert(!!event);
 });
--- reservations/event-publisher/src/index.ts
+++ reservations/event-publisher/src/index.ts
@@ -1,6 +1,22 @@
+import { ValidationError } from "json-schema-to-typescript";
+import { PublicReservationEvent } from "./models/public-reservation-event";
 import { connect } from "./system";
+import validator, { validate } from "./tools/validator";
 
 const targetChannels = ["reservation_events"];
+
+const eventValidator = validator.getSchema<PublicReservationEvent>(
+  "http://example.com/schemas/public-reservation-event.json"
+);
+
+if (!eventValidator) {
+  throw new Error(`Event Validator could not be found.`);
+}
+
+// TODO - Validate outgoing events
+// TODO - Publish also the requested invalid reservations (no schema verification is possible)
+// TODO - See the information that usually is part of the events headers
+// TODO - Add Awaiting events in the BDD tests
 
 connect()
   .then(async (dependencies) => {
@@ -14,8 +30,31 @@ connect()
       );
       console.log("Event:", event);
 
+      // Make sure that the event is valid before publishing out of the bounded context
+      const [validEvent, err] = validate(eventValidator, event);
+
+      if (err) {
+        // For now we log the error. Not sure about what to do in this case
+        console.error(
+          `Error validating the event to be published:`,
+          channel,
+          err
+        );
+        return;
+      }
+
+      if (!validEvent) {
+        // For now we log the error. Not sure about what to do in this case
+        console.error(
+          `Error validating the event to be published:`,
+          channel,
+          "unknown error"
+        );
+        return;
+      }
+
       console.log(`Publishing to`, channel);
-      dependencies.broker.publish(channel, event);
+      dependencies.broker.publish(channel, validEvent);
     });
 
     for (let channel of targetChannels) {
